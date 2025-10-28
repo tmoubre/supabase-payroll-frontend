a48592013142d848482567c02e72794dbf2d23fb
tmoubre <160032580+tmoubre@users.noreply.github.com>
2025-10-28 12:32:34 -0500

    Updated Package-Lock

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://oqltxubuduojxsqzdwlz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xbHR4dWJ1ZHVvanhzcXpkd2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMjI0MTgsImV4cCI6MjA3Njc5ODQxOH0.BmdPs1fENL-R5NngjC5NpCNbJf70X4ofBb2cwvqijZY'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://oqltxubuduojxsqzdwlz.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xbHR4dWJ1ZHVvanhzcXpkd2x6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMjI0MTgsImV4cCI6MjA3Njc5ODQxOH0.BmdPs1fENL-R5NngjC5NpCNbJf70X4ofBb2cwvqijZY";
+export const supabase = createClient(supabaseUrl, supabaseKey);
